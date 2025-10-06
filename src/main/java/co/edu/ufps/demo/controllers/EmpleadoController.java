1689dd74daa1d30ce16bdacc30a7ffea580197a1
carlosreneas <carlosreneas@ufps.edu.co>
2025-10-06 11:16:23 -0500

    Inclusion y conexion de capa de servicio

--- src/main/java/co/edu/ufps/demo/controllers/EmpleadoController.java
+++ src/main/java/co/edu/ufps/demo/controllers/EmpleadoController.java
@@ -1,17 +1,23 @@
 package co.edu.ufps.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ufps.demo.services.EmpleadoService;
 
 @RestController
 @RequestMapping("/empleados")
 public class EmpleadoController {
 	
+	@Autowired
+	EmpleadoService empleadoService;
+	
 	@GetMapping()
 	public String getAllEmpleados() {
 		
-		return "All Empleados";
+		return empleadoService.getAllEmpleados();
 		
 	}
 
--- src/main/java/co/edu/ufps/demo/services/EmpleadoService.java
+++ src/main/java/co/edu/ufps/demo/services/EmpleadoService.java
@@ -1,5 +1,12 @@
 package co.edu.ufps.demo.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmpleadoService {
+	
+	public String getAllEmpleados() {
+		return "All empleados ofrom Service";
+	}
 
 }
