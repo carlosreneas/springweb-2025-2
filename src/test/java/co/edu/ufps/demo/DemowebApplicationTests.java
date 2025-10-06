76c282777255c3d6f3a9a340c8a8a577f756b43c
carlosreneas <carlosreneas@ufps.edu.co>
2025-10-06 10:45:03 -0500

    Inicio del Proyecto

---
+++ src/test/java/co/edu/ufps/demo/DemowebApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
