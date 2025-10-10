a4eed6895443499de4ef6a7d64c612079e833b65
crangarita <crangarita@gmail.com>
2025-10-10 11:25:41 -0500

    Inclusión de metodos para eliminar, actualizar y registrar nuevo elemento

--- src/main/java/co/edu/ufps/demo/controllers/EmpleadoController.java
+++ src/main/java/co/edu/ufps/demo/controllers/EmpleadoController.java
@@ -3,8 +3,12 @@ package co.edu.ufps.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,9 +30,30 @@ public class EmpleadoController {
 	}
 	
 	@GetMapping("/{id}")
-	public Empleado getEmpleado(@PathVariable Integer id) {
-		
+	public Empleado getEmpleado(@PathVariable("id") Integer id) {
+
 		return empleadoService.getEmpleado(id);
+		
+	}
+	
+	@PostMapping
+	public Empleado postEmpleado(@RequestBody Empleado empleado) {
+
+		return empleadoService.saveEmpleado(empleado);
+		
+	}
+	
+	@PutMapping("/{id}")
+	public Empleado putEmpleado(@PathVariable("id") Integer id, @RequestBody Empleado empleado) {
+
+		return empleadoService.updateEmpleado(id, empleado);
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public Empleado deleteEmpleado(@PathVariable("id") Integer id) {
+		
+		return empleadoService.deleteEmpleado(id);
 		
 	}
 
--- src/main/java/co/edu/ufps/demo/services/EmpleadoService.java
+++ src/main/java/co/edu/ufps/demo/services/EmpleadoService.java
@@ -3,6 +3,7 @@ package co.edu.ufps.demo.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +21,39 @@ public class EmpleadoService {
 	}
 	
 	
+	public Empleado saveEmpleado(Empleado empleado) {
+		
+		Empleado e = empleadoRepository.save(empleado);
+		return e;
+		
+	}
+	
+	
+	public Empleado updateEmpleado(Integer id, Empleado empleadoParam) {
+		
+		Empleado empleadoActual = empleadoRepository.findById(id).orElse(null);
+		
+		//empleadoActual.setDocumento(empleadoParam.getDocumento());
+		BeanUtils.copyProperties(empleadoParam, empleadoActual);
+		
+		empleadoRepository.save(empleadoActual);
+		return empleadoActual;
+		
+	}
+	
 	public Empleado getEmpleado(Integer id) {
 				
 		return empleadoRepository.findById(id).orElse(null);
+	}
+	
+	public Empleado deleteEmpleado(Integer id) {
+		
+		Empleado empleado = empleadoRepository.findById(id).orElse(null);
+
+		empleadoRepository.deleteById(id);
+		
+		return empleado;
+				
 	}
 	
 	
